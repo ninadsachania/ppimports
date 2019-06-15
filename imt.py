bbf6693018ffc8b9a35d56be2c85fff99556ac49
Ninad Sachania <ninad.sachania@gmail.com>
2019-06-15 22:57:49 +0530

    Init commit

---
+++ imt.py
@@ -0,0 +1,43 @@
+#! /usr/bin/python3
+
+# Author: Ninad Sachania
+# Aim: Takes all the `import mod[, mod]*` lines out of a .py file and sorts them via their length.
+# It serves no other purpose than to make the import lines at the top of a .py file pretty and organized.
+
+import sys
+import re
+
+
+def main():
+    filename = ' '.join(sys.argv[1:])
+    f = open(filename, 'r')
+
+    r = re.compile('^import*')
+
+    lines = []
+
+    for line in f:
+        if r.match(line.strip()):
+            lines.append(line.strip().rstrip('\n'))
+
+    # an empty set
+    imports = set()
+
+    # remove the lines
+    for i in lines:
+        imts = i.split(' ')[1:]
+
+        for imt in imts:
+            imports.add(imt.rstrip(','))
+            # print(imt.rstrip(','))
+
+    # sort according to length of the module name
+    modules = [x for x in imports]
+    modules.sort(key=len)
+
+    for x in modules:
+        print('import ' + str(x))
+
+
+if __name__ == '__main__':
+    main()
---
+++ test.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import re
+import flask
+import requests
+import pygame
+import random
+import math
+
+import os, sys, imp
+
+try:
+    import sqlite3
+except:
+    pass
+finally:
+    pass
+
+
+def main():
+    # Doesn't work on comments
+    # import mate
+    print('Hello World')
+
+if __name__ == '__main__':
+    main()
